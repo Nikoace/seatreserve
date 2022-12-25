2223a989b0cc638457ac1a734d006540bdcd701e
Kyo <32727925+Nikoace@users.noreply.github.com>
2022-12-25 22:56:47 +0800

    Initial commit

---
+++ src/main/java/com/kaimadata/seatreserve/SeatreserveApplication.java
@@ -0,0 +1,13 @@
+package com.kaimadata.seatreserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatreserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatreserveApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/kaimadata/seatreserve/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.kaimadata.seatreserve;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SeatreserveApplication.class);
+	}
+
+}
